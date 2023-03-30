729abfd389f5b647e2ae5fd76f2b2361c8d837f1
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-03-30 10:16:03 -0300

    Aplicando conceitos de encapsulamento e propriedades.

--- conta.py
+++ conta.py
@@ -1,16 +1,36 @@
 class Conta():
     def __init__(self, numero, titular, saldo, limite): #Função que faz o papel de constutora no Python
         print("Construindo objeto {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo {} do titular {}".format(self.saldo, self.titular))
+        print("O saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor,contaDestino):
+        self.saca(valor)
+        contaDestino.deposita(valor)
+
+    def getSaldo(self):
+        return self.__saldo
+
+    def getTitular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    def getNumeroConta(self):
+        return self.__numero
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
