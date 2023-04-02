4f970cb2ed123fe42ed1d7c47a1f2ac92dcb9cc9
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-04-02 11:05:49 -0300

    Aplicando o conceito de Polimorfismo em modelo.py

--- modelo.py
+++ modelo.py
@@ -15,24 +15,31 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano}: {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min: {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporada} temporadas: {self._likes} Likes'
+
 
 
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao}'
-      f' - Likes: {vingadores.likes}')
-
 
 atlanta = Serie('Atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporada}'
-      f' - Likes: {vingadores.likes}')
 
+filmes_e_series =[vingadores,atlanta]
+for programa in filmes_e_series:
+    print(programa)
